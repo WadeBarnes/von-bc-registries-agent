961880c052783da86d3862b1f1dacdf2ae496dc0
Wade Barnes <wade.barnes@shaw.ca>
2018-09-20 19:28:15 -0700

    Add settings for the tob-tmp prod environment.
    - Remove bcreg-x-proxy

---
+++ openshift/settings.prod-tmp.sh
@@ -0,0 +1,16 @@
+export TOOLS="devex-von-bc-registries-agent-tools"
+export PROJECT_NAMESPACE="devex-bcgov-dap"
+
+# Used for tmp deployment scripts
+export images="postgresql postgresql-oracle-fdw bcreg-x-agent mara schema-spy-with-oracle-jdbc"
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Danger, Danger, Will Robinson!
+# ----------------------------------------------
+# Override environments, since there is only one:
+# devex-bcgov-dac-dev
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+export DEPLOYMENT_ENV_NAME="dev"
+export DEV="dev"
+export TEST="dev"
+export PROD="dev"
